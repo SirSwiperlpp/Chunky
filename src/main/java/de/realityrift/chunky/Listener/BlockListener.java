f7b3da2585012ba4a5b379d3aeef9e5d321ae373
SirSwiperlpp <damonkaintoch73@icloud.com>
2023-12-10 21:31:14 +0100

    EndCrystals Now dont damage there seroundings

--- src/main/java/de/realityrift/chunky/Listener/BlockListener.java
+++ src/main/java/de/realityrift/chunky/Listener/BlockListener.java
@@ -3,10 +3,14 @@ package de.realityrift.chunky.Listener;
 import de.realityrift.chunky.Lang.Language;
 import de.realityrift.chunky.Main.Main;
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryInteractEvent;
@@ -31,37 +35,25 @@ public class BlockListener implements Listener {
     }
 
     @EventHandler
-    public void onCrystalCraft(CraftItemEvent event) {
+    public void onCrystalDamage(EntityDamageEvent event)
+    {
         if (!Main.config.getBoolean("disable-crystal")) return;
+        Entity entity = event.getEntity();
 
-        if (event.getRecipe() != null && event.getRecipe().getResult().getType() == Material.END_CRYSTAL) {
-            event.getWhoClicked().sendMessage(language.get("prefix") + language.translateString("block.disabled", String.valueOf(event.getRecipe().getResult().getType())));
+        if (entity.equals(EntityType.ENDER_CRYSTAL))
+        {
             event.setCancelled(true);
         }
     }
 
     @EventHandler
-    public void onPlayerPickupItem(PlayerPickupItemEvent event) {
-        if (event.getItem().getItemStack().getType() == Material.END_CRYSTAL) {
-            Player player = event.getPlayer();
+    public void onCrystalExplode(EntityExplodeEvent event)
+    {
+        if (!Main.config.getBoolean("disable-crystal")) return;
+        Entity entity = event.getEntity();
 
-            player.getInventory().addItem(
-                    new ItemStack(Material.GLASS, 9),
-                    new ItemStack(Material.ENDER_EYE, 1),
-                    new ItemStack(Material.GHAST_TEAR, 1)
-            );
-
-            event.getPlayer().getInventory().remove(Material.END_CRYSTAL);
-
-            player.sendMessage(language.get("prefix") + language.translateString("block.disabled", String.valueOf(event.getItem().getItemStack().getType())));
-        }
-    }
-
-    @EventHandler
-    public void onEndCrystalDrop(PlayerDropItemEvent event) {
-        if (event.getItemDrop().getItemStack().getType() == Material.END_CRYSTAL)
+        if (entity.getType().equals(EntityType.ENDER_CRYSTAL))
         {
-            event.getPlayer().sendMessage(language.get("prefix") + language.translateString("block.disabled", String.valueOf(event.getItemDrop().getItemStack().getType())));
             event.setCancelled(true);
         }
     }
